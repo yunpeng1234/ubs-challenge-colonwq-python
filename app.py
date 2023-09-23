29c894f30cab4f0a748c45d6ff71a54807c73d83
yunpeng1234 <xiao.yunpeng99@gmail.com>
2023-09-23 12:09:29 +0800

    Do airport

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from routes.challenge1.lazydev import lazydev
 from routes.challenge2.greedymonkey import greedymonkey
 from routes.challenge3.digitalcolony import digitalcolony
-
+from routes.airport import airport
 
 import logging
 
@@ -18,14 +18,15 @@ def default_route():
 app.register_blueprint(lazydev)
 app.register_blueprint(greedymonkey)
 app.register_blueprint(digitalcolony)
+app.register_blueprint(airport)
 
-logger = logging.getLogger()
-handler = logging.StreamHandler()
-formatter = logging.Formatter("%(asctime)s %(name)-12s %(levelname)-8s %(message)s")
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-logger.setLevel(logging.INFO)
+# logger = logging.getLogger()
+# handler = logging.StreamHandler()
+# formatter = logging.Formatter("%(asctime)s %(name)-12s %(levelname)-8s %(message)s")
+# handler.setFormatter(formatter)
+# logger.addHandler(handler)
+# logger.setLevel(logging.INFO)
 
-if __name__ == "__main__":
-    logging.info("Starting application ...")
-    app.run(port=8080, debug=True)
+# if __name__ == "__main__":
+#     logging.info("Starting application ...")
+#     app.run(port=8080, debug=True)
---
+++ routes/airport.py
@@ -0,0 +1,119 @@
+from flask import Blueprint, jsonify, request
+
+airport = Blueprint("airport", __name__)
+
+
+# Simple passenger class
+class Passenger:
+    def __init__(self, departureTime):
+        self.departureTime = departureTime
+        self.numberOfRequests = 0
+
+    def askTimeToDeparture(self):
+        self.numberOfRequests += 1
+        return self.departureTime
+
+    def getNumberOfRequests(self):
+        return self.numberOfRequests
+
+
+def execute(prioritisation_function, passenger_data, cut_off_time, test_id):
+    totalNumberOfRequests = 0
+    passengers = []
+
+    # Initialise list of passenger instances
+    for i in range(len(passenger_data)):
+        passengers.append(Passenger(passenger_data[i]))
+
+    # Apply solution and re-shuffle with departure cut-off time
+    prioritised_and_filtered_passengers = prioritisation_function(
+        passengers, cut_off_time
+    )
+
+    # Sum totalNumberOfRequests across all passengers
+    for i in range(len(passengers)):
+        totalNumberOfRequests += passengers[i].getNumberOfRequests()
+    print("totalNumberOfRequests: " + str(totalNumberOfRequests))
+
+    # Print sequence of sorted departure times
+    print("Sequence of prioritised departure times:")
+    prioritised_filtered_list = []
+    for i in range(len(prioritised_and_filtered_passengers)):
+        print(prioritised_and_filtered_passengers[i].departureTime, end=" ")
+        prioritised_filtered_list.append(
+            prioritised_and_filtered_passengers[i].departureTime
+        )
+
+    print("\n")
+    return {
+        "id": test_id,
+        "total_number_of_requests": totalNumberOfRequests,
+        "prioritised_filtered_list": prioritised_filtered_list,
+    }
+
+
+def mergeSort(arr):
+    if len(arr) > 1:
+        # Finding the mid of the array
+        mid = len(arr) // 2
+        # Dividing the array elements
+        L = arr[:mid]
+
+        # Into 2 halves
+        R = arr[mid:]
+
+        # Sorting the first half
+        mergeSort(L)
+
+        # Sorting the second half
+        mergeSort(R)
+
+        i = j = k = 0
+
+        while i < len(L) and j < len(R):
+            if L[i].askTimeToDeparture() <= R[j].askTimeToDeparture():
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+
+        # Checking if any element was left
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+
+
+def prioritisation_function(passengers, cut_off_time):
+    okToUse = []
+    for p in passengers:
+        if p.askTimeToDeparture() <= cut_off_time:
+            okToUse.append(p)
+    mergeSort(passengers)
+    # your solution here
+    # return sorted array of passenger instances
+    return passengers
+
+
+@airport.route("/airport", methods=["POST"])
+def getCommon():
+    w = request.json
+    print(w)
+    arrayReq = []
+    for o in w:
+        arrayReq.append(
+            execute(
+                prioritisation_function,
+                o["departureTimes"],
+                o["cutOffTime"],
+                o["id"],
+            )
+        )
+    return jsonify(arrayReq)
