854ac680508244484b9d14876b8bcd16ef347614
yunpeng1234 <xiao.yunpeng99@gmail.com>
2023-09-23 12:33:52 +0800

    Remove debug

--- app.py
+++ app.py
@@ -20,13 +20,13 @@ app.register_blueprint(greedymonkey)
 app.register_blueprint(digitalcolony)
 app.register_blueprint(airport)
 
-logger = logging.getLogger()
-handler = logging.StreamHandler()
-formatter = logging.Formatter("%(asctime)s %(name)-12s %(levelname)-8s %(message)s")
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-logger.setLevel(logging.INFO)
+# logger = logging.getLogger()
+# handler = logging.StreamHandler()
+# formatter = logging.Formatter("%(asctime)s %(name)-12s %(levelname)-8s %(message)s")
+# handler.setFormatter(formatter)
+# logger.addHandler(handler)
+# logger.setLevel(logging.INFO)
 
-if __name__ == "__main__":
-    logging.info("Starting application ...")
-    app.run(port=8080, debug=True)
+# if __name__ == "__main__":
+#     logging.info("Starting application ...")
+#     app.run(port=8080, debug=True)
