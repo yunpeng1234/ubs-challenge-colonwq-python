8ee5ecc3cbd42d2d6925f5b747d359438965bab0
yunpeng1234 <xiao.yunpeng99@gmail.com>
2023-09-23 16:00:56 +0800

    Remove local dev

--- app.py
+++ app.py
@@ -21,13 +21,13 @@ app.register_blueprint(digitalcolony)
 app.register_blueprint(airport)
 app.register_blueprint(swissbyte)
 
-logger = logging.getLogger()
-handler = logging.StreamHandler()
-formatter = logging.Formatter("%(asctime)s %(name)-12s %(levelname)-8s %(message)s")
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-logger.setLevel(logging.INFO)
+# logger = logging.getLogger()
+# handler = logging.StreamHandler()
+# formatter = logging.Formatter("%(asctime)s %(name)-12s %(levelname)-8s %(message)s")
+# handler.setFormatter(formatter)
+# logger.addHandler(handler)
+# logger.setLevel(logging.INFO)
 
-if __name__ == "__main__":
-    logging.info("Starting application ...")
-    app.run(port=8080, debug=True)
+# if __name__ == "__main__":
+#     logging.info("Starting application ...")
+#     app.run(port=8080, debug=True)
