ca31a7594c950891c45679e86eff1b4cef665f46
yunpeng1234 <xiao.yunpeng99@gmail.com>
2023-09-24 01:54:22 +0800

    Push arcs

--- routes/piechart.py
+++ routes/piechart.py
@@ -3,6 +3,8 @@ from math import pi
 
 piechart = Blueprint("piechart", __name__)
 
+MIN_CHORD = pi / 1000
+
 
 def counting(res):
     prev = res[0]
@@ -13,19 +15,32 @@ def counting(res):
     return res
 
 
-def calculateRadians(total, qtys, maxRadians):
+def calculateRadiansSmallArc(total, qtys, maxRadians, starting):
     qtys.sort()
-    res = [0.0]
+    res = [starting]
     for qty in qtys:
-        if qty / total < 0.0005:
-            counter += 1
-            res.append(res[-1] - 0.00314159)
-            maxRadians -= 0.00314159
+        if qty / total * maxRadians < MIN_CHORD:
+            res.append(res[-1] - MIN_CHORD)
+            maxRadians -= MIN_CHORD
             total -= qty
         else:
             res.append(res[-1] - ((qty / total) * maxRadians))
-    counting(res)
-    print(res)
+
+    newR = list(map(lambda x: round(x, 8), res))
+    return newR
+
+
+def calculateRadians(total, qtys, maxRadians):
+    qtys.sort()
+    res = [7 / 6 * pi]
+    for qty in qtys:
+        if qty / total < 0.0005:
+            res.append(res[-1] + MIN_CHORD)
+            maxRadians -= MIN_CHORD
+            total -= qty
+        else:
+            res.append(res[-1] + ((qty / total) * maxRadians))
+
     newR = list(map(lambda x: round(x, 8), res))
     return {"instruments": newR}
 
@@ -38,8 +53,8 @@ def calculateRadians2(total, qtys, maxRadians):
     for qty in qtys:
         if qty / total < 0.0005:
             counter += 1
-            res.append(res[-1] - 0.00314159)
-            maxRadians -= 0.00314159
+            res.append(res[-1] - MIN_CHORD)
+            maxRadians -= MIN_CHORD
             total -= qty
         else:
             res.append(res[-1] - ((qty / total) * maxRadians))
@@ -49,64 +64,55 @@ def calculateRadians2(total, qtys, maxRadians):
     return {"instruments": newR[::-1]}
 
 
-def checkMinimum(total, min, checkAgainst):
-    if (min / total) <= 1 / 1000:
-        newArcSize = (1 / 1000 * pi) / min * total
-        return (False, newArcSize)
-    return (True, 0)
+# def checkMinimum(total, min, checkAgainst):
+#     if (min / total) <= 1 / 1000:
+#         newArcSize = (1 / 1000 * pi) / min * total
+#         return (False, newArcSize)
+#     return (True, 0)
 
 
 def calcSplitChord(total, qty, cm, am, rm, sm):
-    res = {}
     # Calc instruments
-    rhsArcSize = (2 / 3 - 3 / 1000) * pi / 4
 
-    res = calculateRadians(total, qty, maxRadians=pi * 2 / 3)["instruments"][::-1]
-    qty = list(map(lambda x: x + (pi * (7 / 6)), tempR))
-    res["instruments"] = qty
-
+    res = calculateRadians(total, qty, maxRadians=pi * 2 / 3)
+    print(res)
     # Get the smallest for each given arc
     cMin = min(cm.values())
     aMin = min(cm.values())
     rMin = min(rm.values())
     sMin = min(sm.values())
 
-    tempH = {}
-    tempH["c"] = cMin
-    tempH["a"] = aMin
-    tempH["r"] = rMin
-    tempH["s"] = sMin
-    totalArcSize = (2 / 3 - 3 / 1000) * pi
-    count = 4
-    arcSize = {}
-    for k, v in sorted(tempH.items(), key=lambda item: item[1]):
-        ok, newArc = checkMinimum(total, v, totalArcSize / count)
-        print(newArc)
-        if not ok:
-            totalArcSize -= newArc
-            arcSize[k] = newArc
-            count -= 1
-        else:
-            arcSize[k] = totalArcSize / count
+    # tempH = {}
+    # tempH["c"] = cMin
+    # tempH["a"] = aMin
+    # tempH["r"] = rMin
+    # tempH["s"] = sMin
+    # totalArcSize = (2 / 3 - 3 / 1000) * pi
+    # count = 4
+    # arcSize = {}
+    # for k, v in sorted(tempH.items(), key=lambda item: item[1]):
+    #     ok, newArc = checkMinimum(total, v, totalArcSize / count)
+    #     print(newArc)
+    #     if not ok:
+    #         totalArcSize -= newArc
+    #         arcSize[k] = newArc
+    #         count -= 1
+    #     else:
+    #         arcSize[k] = totalArcSize / count
+    rhsArcSize = (2 / 3 - 3 / 1000) * pi / 4
+    print(rhsArcSize)
+    starts = [(1 / 6) * pi + rhsArcSize]
+    for i in range(3):
+        starts.append(starts[-1] + rhsArcSize + MIN_CHORD)
+    print(starts)
     order = ["c", "s", "a", "r"]
     tag = ["currency", "sector", "assetClass", "region"]
     va = [cm, sm, am, rm]
     currHeader = 1 / 6 * pi
     for i in range(4):
-        arcS = arcSize[order[i]]
         nums = list(va[i].values())
-        temp = calculateRadians(total, nums, arcS)["instruments"]
-        tmp = list(
-            map(
-                lambda x: round(x, 8),
-                map(
-                    lambda x: x + currHeader,
-                    temp,
-                ),
-            )
-        )
-        res[tag[i]] = tmp
-        currHeader += arcS + (1 / 1000 * pi)
+        temp = calculateRadiansSmallArc(total, nums, rhsArcSize, starts[i])
+        res[tag[i]] = temp[::-1]
     return res
 
 
@@ -149,5 +155,7 @@ def getCommon():
     if isFirst:
         return jsonify(calculateRadians2(total, counts, 2 * pi))
     else:
-        return jsonify({})
-        # calcSplitChort(total, counts, currency, assetClass, region, sector)
+        return jsonify(
+            calcSplitChord(total, counts, currency, assetClass, region, sector)
+        )
+        #
