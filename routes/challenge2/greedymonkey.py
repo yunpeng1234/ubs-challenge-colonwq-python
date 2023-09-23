1c43c478245dd645f045c1145cc2d31f01b79de3
yunpeng1234 <xiao.yunpeng99@gmail.com>
2023-09-23 09:49:39 +0800

    Test greedy monkey

---
+++ routes/challenge2/greedymonkey.py
@@ -0,0 +1,37 @@
+from flask import Blueprint, render_template, session, abort, request, jsonify
+
+
+greedymonkey = Blueprint("greedymonkey", __name__)
+
+
+def getFruit(max_weight, max_volume, fruit):
+    n = len(fruit)
+    dp = [[[0] * (max_volume + 1) for _ in range(max_weight + 1)] for _ in range(n + 1)]
+
+    for i in range(n + 1):
+        for w in range(max_weight + 1):
+            for v in range(max_volume + 1):
+                weight, volume, value = fruit[i - 1]
+                if i == 0 or w == 0 or v == 0:
+                    dp[i][w][v] = 0
+                elif weight <= w and volume <= v:
+                    dp[i][w][v] = max(
+                        dp[i - 1][w][v],
+                        dp[i - 1][w - weight][v - volume] + value,
+                    )
+                else:
+                    dp[i][w][v] = dp[i - 1][w][v]
+    return dp[-1][-1][-1]
+
+
+# f = [[110, 80, 60], [80, 155, 90]]
+# print(getFruit(100, 150, f))
+
+
+@greedymonkey.route("/greedyMonkey", methods=["POST"])
+def getCommon():
+    w = request.json["w"]
+    v = request.json["v"]
+    f = request.json["v"]
+
+    return getFruit(w, v, f)
