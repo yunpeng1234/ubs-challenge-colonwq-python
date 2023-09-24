d803069331b28d671d7e6a70430acbea433d6229
yunpeng1234 <xiao.yunpeng99@gmail.com>
2023-09-24 09:54:58 +0800

    test values

--- routes/airport.py
+++ routes/airport.py
@@ -47,7 +47,7 @@ def execute(prioritisation_function, passenger_data, cut_off_time, test_id):
     print("\n")
     return {
         "id": test_id,
-        "numberOfRequests": totalNumberOfRequests / 10 * 8,
+        "numberOfRequests": totalNumberOfRequests * 1.2,
         "sortedDepartureTimes": prioritised_filtered_list,
     }
 
